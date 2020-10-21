f00a5d874cd6246448bc47fbad8e2328577be63f
vudinhquang <vudinhquangk53@gmail.com>
2020-10-21 22:49:44 +0900

    Wp-config 04

--- new_config.php
+++ new_config.php
@@ -1,7 +1,14 @@
 <?php
 define( 'WP_SITEURL', 'http://wordpress.xyz' );
 define( 'WP_HOME', 'http://wordpress.xyz' );
+
 define( 'WP_PLUGIN_DIR', dirname(__FILE__) . '/wp-content/plugins');
 define( 'WP_PLUGIN_URL', 'http://wordpress.xyz/wp-content/plugins');
+
 // define( 'WP_POST_REVISIONS', false );
 define( 'WP_POST_REVISIONS', 3 );
+define( 'AUTOSAVE_INTERVAL', 70 );
+define( 'SAVEQUERIES', true );
+
+define( 'WP_MEMORY_LIMIT', '128M' );
+define ( 'WPLANG', 'en-US' );
