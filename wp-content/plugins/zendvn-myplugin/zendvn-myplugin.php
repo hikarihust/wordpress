72b9223c6bd971dc8a6b0b3c1b87d0febbc50ae5
vudinhquang <vudinhquangk53@gmail.com>
2020-10-22 17:46:04 +0900

    Remove, remove_all, has_action

--- wp-content/plugins/zendvn-myplugin/zendvn-myplugin.php
+++ wp-content/plugins/zendvn-myplugin/zendvn-myplugin.php
@@ -8,19 +8,5 @@ Version: 1.0
 Author URI: http://www.zend.vn
 */
 
-add_action('wp_footer', 'zendvn_mp_footer');
-
-function zendvn_mp_footer(){
-	echo "<br /> Page: " . is_page();
-}
-
-add_action('wp_head', 'zendvn_mp_new_css');
-
-function zendvn_mp_new_css(){
-	if(is_page() == true){
-		$cssUrl = plugins_url('/css/abc.css',__FILE__);
-		$css = '<link rel="stylesheet" type="text/css" media="all" href="' . $cssUrl . '" />';
-		echo $css;
-	}
-	
-}
+// remove_all_actions('wp_head',20);
+echo '<br/>' . has_action( 'wp_head', 'feed_links' );
