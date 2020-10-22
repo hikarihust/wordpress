e0b022e8f1e90068b5da0f159bd4ecb9c1b826c5
vudinhquang <vudinhquangk53@gmail.com>
2020-10-23 02:22:11 +0900

    Add_filter-the_title

--- wp-content/plugins/zendvn-myplugin/includes/public.php
+++ wp-content/plugins/zendvn-myplugin/includes/public.php
@@ -7,7 +7,17 @@ class ZendvnMp{
 		//=====================================================
 		//1. Ham thay doi toan bo title trong hook 'the_title'
 		//=====================================================
-		add_filter('the_title', array($this,'theTitle'));
+		// add_filter('the_title', array($this,'theTitle'));
+
+		//=====================================================
+		//2. Ham su dung 2 tham cua hook 'the_title'
+		//=====================================================
+		// add_filter('the_title', array($this,'theTitle2'),10,2);
+
+		//=====================================================
+		//3. Ham su dung 2 tham cua hook 'the_title'
+		//=====================================================
+		add_filter('the_title', array($this,'theTitle3'),10,2);
 	}
 
 	//=====================================================
@@ -16,6 +26,28 @@ class ZendvnMp{
 	public function theTitle(){
 		$str = 'Thay doi tieu cua bai viet';
 		return $str;
+	}
+
+	//=====================================================
+	//2. Ham su dung 2 tham cua hook 'the_title'
+	//=====================================================
+	public function theTitle2($title, $id){
+		//echo "<br/>" . $id;
+		if($id == 1){
+			$title = str_replace("Hello", "Xin chào", $title);
+		}
+		return $title;
+	}
+
+	//=====================================================
+	//3. Ham su dung 2 tham cua hook 'the_title'
+	//=====================================================
+	public function theTitle3($title, $id){
+		//echo "<br/>" . $id;
+		if($id == 1){
+			$title = 'Chào mừng các bạn đến với thế giới WP';
+		}
+		return $title;
 	}
 
 }
