8bc5226c35dca3369ac2cacdb62b1a5e9b9f4d49
vudinhquang <vudinhquangk53@gmail.com>
2020-10-23 15:40:43 +0900

    Remove_menu - remove_submenu

--- wp-content/plugins/zendvn-myplugin/admin.php
+++ wp-content/plugins/zendvn-myplugin/admin.php
@@ -6,7 +6,9 @@ class ZendvnMpAdmin {
         // echo '<br>' . __METHOD__;
         // add_action('admin_menu', array($this,'settingMenu'));
         // add_action('admin_menu', array($this,'settingMenu2'));
-        add_action('admin_menu', array($this,'settingMenu3'));
+        // add_action('admin_menu', array($this,'settingMenu3'));
+        add_action('admin_menu', array($this,'settingMenu4'));
+        // add_action('admin_menu', array($this,'removeMenu'));
     }
     
 	//=======================================================
@@ -46,8 +48,38 @@ class ZendvnMpAdmin {
 		add_submenu_page($menuSlug, "Help title", "Help", 'manage_options',
 						$menuSlug . '-help',array($this,'helpPage'));
 	
-	}
+    }
+    
+	//=======================================================
+	//4. Them mot nhom menu moi vao vi tri bat ky
+	//=======================================================
+	public function settingMenu4(){
+		$menuSlug = 'zendvn-mp-my-setting';
+		add_menu_page('My Setting title', 'My Setting', 'manage_options',
+						$menuSlug,array($this,'settingPage'),
+						ZENDVN_MP_IMAGES_URL . '/icon-setting16x16.png',1);
 	
+		add_submenu_page($menuSlug, "About title", "About", 'manage_options',
+		$menuSlug . '-about',array($this,'aboutPage'));
+	
+		add_submenu_page($menuSlug, "Help title", "Help", 'manage_options',
+		$menuSlug . '-help',array($this,'helpPage'));
+	
+    }
+    
+	//=======================================================
+	//5. Xoa bo cac menu trong he thong menu cua WP
+	//=======================================================
+	public function removeMenu(){
+		$menuSlug = 'zendvn-mp-my-setting';
+		remove_submenu_page( $menuSlug, $menuSlug . '-about' );
+		remove_menu_page( $menuSlug );
+		
+		remove_submenu_page( 'edit.php', 'post-new.php');
+		remove_menu_page( 'edit.php' );
+	
+    }
+    	
 	public function settingPage(){
 		require ZENDVN_MP_VIEWS_DIR . '/setting-page.php';
     }
