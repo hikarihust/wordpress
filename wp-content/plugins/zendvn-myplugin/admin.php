46fe83d0494070c5f1d71df31140ca6d478759d1
vudinhquang <vudinhquangk53@gmail.com>
2020-10-23 19:37:38 +0900

    Get_option

--- wp-content/plugins/zendvn-myplugin/admin.php
+++ wp-content/plugins/zendvn-myplugin/admin.php
@@ -5,7 +5,7 @@ class ZendvnMpAdmin {
 	public function __construct(){
         // echo '<br>' . __METHOD__;
         // add_action('admin_init', array($this,'add_new_option'));
-        add_action('admin_init', array($this,'add_array_option'));
+        // add_action('admin_init', array($this,'add_array_option'));
         /*
         $tmp = 'a:3:{s:6:"course";s:13:"Wordpress 4.x";s:6:"author";s:12:"ZendVN group";s:7:"website";s:11:"www.zend.vn";}';
         $tmp = unserialize($tmp);
@@ -13,6 +13,7 @@ class ZendvnMpAdmin {
         print_r($tmp);
         echo "</pre>";
         */
+        add_action('admin_init', array($this,'get_options'));
     }
 
 	public function add_new_option(){
@@ -28,6 +29,20 @@ class ZendvnMpAdmin {
 				);
 		
 		add_option('zendvn_mp_wp_course',$arrOption,'','yes');
-	}
-     
+    }
+    
+	public function get_options(){
+		$tmp = get_option('zendvn_mp_wp_version','3.0');
+		echo '<br/>' . $tmp . '<br />';
+		
+		$arrOption = array(
+				'course' => 'Wordpress 4.x',
+				'author' => 'ZendVN group',
+				'website' => 'www.zend.vn'
+			);
+		$tmp = get_option('zendvn_mp_wp_course',$arrOption);
+		echo '<pre>';
+		print_r($tmp);
+		echo '</pre>';
+    }     
 }
