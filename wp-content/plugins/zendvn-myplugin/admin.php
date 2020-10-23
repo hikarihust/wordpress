5395bcbf44d8f1d734d19cb60e5508cf8c6843ae
vudinhquang <vudinhquangk53@gmail.com>
2020-10-23 14:52:28 +0900

    Add_menu_page & add_submenu_page

--- wp-content/plugins/zendvn-myplugin/admin.php
+++ wp-content/plugins/zendvn-myplugin/admin.php
@@ -3,8 +3,10 @@ require_once ZENDVN_MP_PLUGIN_DIR . '/includes/support.php';
 class ZendvnMpAdmin {
 
 	public function __construct(){
-        echo '<br>' . __METHOD__;
-        add_action('admin_menu', array($this,'settingMenu'));
+        // echo '<br>' . __METHOD__;
+        // add_action('admin_menu', array($this,'settingMenu'));
+        // add_action('admin_menu', array($this,'settingMenu2'));
+        add_action('admin_menu', array($this,'settingMenu3'));
     }
     
 	//=======================================================
@@ -14,9 +16,48 @@ class ZendvnMpAdmin {
 		$menuSlug = 'zendvn-mp-my-setting';
 		add_posts_page('My Setting title', 'My Setting', 'manage_options', 
 							$menuSlug, array($this,'settingPage'));
+    }
+    
+	//=======================================================
+	//2. Them mot nhom menu moi vao he thong menu WP
+	//=======================================================
+	public function settingMenu2(){
+		$menuSlug = 'zendvn-mp-my-setting';		
+		add_menu_page('My Setting title', 'My Setting', 'manage_options',
+                        $menuSlug,array($this,'settingPage'),
+                        ZENDVN_MP_IMAGES_URL . '/icon-setting16x16.png');
+                        
+        add_menu_page('My Setting 2 title', 'My Setting 2', 'manage_options',
+						$menuSlug . '-2',array($this,'settingPage'));
+    }
+    
+	//=======================================================
+	//3. Them mot nhom menu moi vao he thong menu WP
+	//=======================================================
+	public function settingMenu3(){
+		$menuSlug = 'zendvn-mp-my-setting';
+		add_menu_page('My Setting title', 'My Setting', 'manage_options',
+						$menuSlug,array($this,'settingPage'),
+						ZENDVN_MP_IMAGES_URL . '/icon-setting16x16.png');
+		
+		add_submenu_page($menuSlug, "About title", "About", 'manage_options', 
+						$menuSlug . '-about',array($this,'aboutPage'));
+						
+		add_submenu_page($menuSlug, "Help title", "Help", 'manage_options',
+						$menuSlug . '-help',array($this,'helpPage'));
+	
 	}
 	
 	public function settingPage(){
-		echo '<h2>My Setting</h2>';
+		require ZENDVN_MP_VIEWS_DIR . '/setting-page.php';
+    }
+    
+	public function aboutPage(){
+		require ZENDVN_MP_VIEWS_DIR . '/about-page.php';
+    }
+
+	public function helpPage(){
+		require ZENDVN_MP_VIEWS_DIR . '/help-page.php';
 	}
+    
 }
--- wp-content/plugins/zendvn-myplugin/zendvn-myplugin.php
+++ wp-content/plugins/zendvn-myplugin/zendvn-myplugin.php
@@ -9,7 +9,10 @@ Author URI: http://www.zend.vn
 */
 
 define('ZENDVN_MP_PLUGIN_URL', plugin_dir_url(__FILE__));
+define('ZENDVN_MP_IMAGES_URL', ZENDVN_MP_PLUGIN_URL . '/images');
+
 define('ZENDVN_MP_PLUGIN_DIR', plugin_dir_path(__FILE__));
+define('ZENDVN_MP_VIEWS_DIR', ZENDVN_MP_PLUGIN_DIR . '/views');
 
 if(!is_admin()){
 	require_once ZENDVN_MP_PLUGIN_DIR . '/public.php';
