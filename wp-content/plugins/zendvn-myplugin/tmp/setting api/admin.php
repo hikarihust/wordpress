fe7056c66f5512ef34d0cde19747ade55d7dc206
vudinhquang <vudinhquangk53@gmail.com>
2020-10-24 12:47:03 +0900

    Save & show setting 1

--- wp-content/plugins/zendvn-myplugin/tmp/setting api/admin.php
+++ wp-content/plugins/zendvn-myplugin/tmp/setting api/admin.php
@@ -4,8 +4,12 @@ class ZendvnMpAdmin {
 
 	private $_menuSlug = 'zendvn-mp-my-setting';
 
+	private $_setting_options;
+
 	public function __construct(){
 		// echo '<br>' . __METHOD__;
+
+		$this->_setting_options = get_option('zendvn_mp_name',array());
 		add_action('admin_menu', array($this,'settingMenu'));
 
 		add_action('admin_init', array($this,'register_setting_and_fields'));
@@ -18,16 +22,22 @@ class ZendvnMpAdmin {
 		$mainSection = 'zendvn_mp_main_section';
 		add_settings_section($mainSection, "Main setting", 
 							array($this,'main_section_view'), $this->_menuSlug);
-
 		add_settings_field('zendvn_mp_new_title', 'Site title', array($this,'create_form'), 
 							$this->_menuSlug,$mainSection,array('name'=>'new_title_input'));	
+
+		// EXT SETTING
+		$extSection = 'zendvn_mp_ext_section';
+		add_settings_section($extSection, "Ext setting", 
+							array($this,'main_section_view'), $this->_menuSlug);
+		add_settings_field('zendvn_mp_slogan', 'Slogan:', array($this,'create_form'),
+							$this->_menuSlug,$extSection, array('name'=>'slogan_input'));
 	}
 
 	//===============================================
 	//Kiem tra cac dieu kien truoc khi luu du lieu vao database
 	//===============================================
 	public function validate_setting($data_input) {
-
+		return $data_input;
 	}
 
 	public function main_section_view(){
@@ -38,7 +48,13 @@ class ZendvnMpAdmin {
 				
 		if($args['name']== 'new_title_input'){
 			echo '<input type="text" name="zendvn_mp_name[zendvn_mp_new_title]"
-						value=""/>';
+						value="' . $this->_setting_options['zendvn_mp_new_title'] . '"/>';
+			echo '<p class="description">Nhập vào một chuỗi không quá 20 ký tự</p>';
+		}
+
+		if($args['name']== 'slogan_input'){
+			echo '<input type="text" name="zendvn_mp_name[zendvn_mp_slogan]"
+						value="' . $this->_setting_options['zendvn_mp_slogan'] . '"/>';
 			echo '<p class="description">Nhập vào một chuỗi không quá 20 ký tự</p>';
 		}
 		
