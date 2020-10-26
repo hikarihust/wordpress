faa50d9f00152129a0680518b8b3dd662c175678
vudinhquang <vudinhquangk53@gmail.com>
2020-10-26 14:58:22 +0900

    Add JS 1

--- wp-content/plugins/zendvn-myplugin/zendvn-myplugin.php
+++ wp-content/plugins/zendvn-myplugin/zendvn-myplugin.php
@@ -11,6 +11,7 @@ Author URI: http://www.zend.vn
 define('ZENDVN_MP_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('ZENDVN_MP_IMAGES_URL', ZENDVN_MP_PLUGIN_URL . '/images');
 define('ZENDVN_MP_CSS_URL', ZENDVN_MP_PLUGIN_URL . 'css');
+define('ZENDVN_MP_JS_URL', ZENDVN_MP_PLUGIN_URL . 'js');
 
 define('ZENDVN_MP_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('ZENDVN_MP_VIEWS_DIR', ZENDVN_MP_PLUGIN_DIR . '/views');
