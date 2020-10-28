cb6fcc0a2d01b2f1bd961e00353b60d42b1d3704
vudinhquang <vudinhquangk53@gmail.com>
2020-10-28 11:33:34 +0900

    Shortcode API 3

--- wp-content/plugins/zendvn-myplugin/shortcodes/date.php
+++ wp-content/plugins/zendvn-myplugin/shortcodes/date.php
@@ -7,6 +7,10 @@ class Zendvn_Mp_SC_Date{
 	
 	public function show(){
 		
+		$postObj = get_post(get_the_ID());
+		if(has_shortcode($postObj->post_content, 'zendvn_mp_sc_date') == 1){
+			wp_enqueue_style('zendvn_mp_sc_date_css',ZENDVN_MP_CSS_URL . '/abc.css', array(),'1.0');
+		}
         $str = '<div class="zendvn_mp_sc_date_css">'
         . date('l jS \of F Y h:i:s A')
         . '</div>';
--- wp-content/plugins/zendvn-myplugin/shortcodes/main.php
+++ wp-content/plugins/zendvn-myplugin/shortcodes/main.php
@@ -6,18 +6,23 @@ class Zendvn_Mp_SC_Main{
 
 	public function __construct(){
 		$defaultOption = array(
-					'zendvn_mp_sc_date' => false
+					'zendvn_mp_sc_date' => true
                 );
                 
         $this->_shortcode_option = get_option($this->_shortcode_name,$defaultOption);
 
-        $this->date();
+		$this->date();
+		
+		// remove_shortcode("zendvn_mp_sc_date");
+		// echo "<br />shortcode_exists: " . shortcode_exists("zendvn_mp_sc_date");
     }
     
 	public function date(){
 		if($this->_shortcode_option['zendvn_mp_sc_date'] == true){
 			require_once ZENDVN_MP_SHORTCODE_DIR . '/date.php';
 			new Zendvn_Mp_SC_Date();
+		} else {
+			add_shortcode('zendvn_mp_sc_date', '__return_false');
 		}
 	}
 	
