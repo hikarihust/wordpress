075ba90aa5cf6f4af16328d0915bfc18ca5d6437
vudinhquang <vudinhquangk53@gmail.com>
2020-10-30 15:56:06 +0900

    Loop 2

--- wp-content/themes/twentyten-child/loop.php
+++ wp-content/themes/twentyten-child/loop.php
@@ -3,6 +3,23 @@
     // print_r($wp_query);
     // echo "</pre>";
 ?>
+<style>
+.zendvn-loop{
+	list-style-type: none;
+}
+
+.zendvn-loop li{
+	border: 1px solid #ccc;
+	padding: 8px;
+	margin-bottom: 10px;
+}
+</style>
+<?php if ( $wp_query->max_num_pages > 1 ) : ?>
+				<div id="nav-below" class="navigation">
+					<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'twentyten' ) ); ?></div>
+					<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'twentyten' ) ); ?></div>
+				</div><!-- #nav-below -->
+<?php endif; ?>
 
 <?php 
     if( have_posts()){
@@ -20,8 +37,17 @@
         </li>
 <?php
         } 
+        // wp_reset_query();
+        wp_reset_postdata();
         echo "</ul>";
     } else {
         
     } 
 ?>
+
+<?php if ( $wp_query->max_num_pages > 1 ) : ?>
+				<div id="nav-below" class="navigation">
+					<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'twentyten' ) ); ?></div>
+					<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'twentyten' ) ); ?></div>
+				</div><!-- #nav-below -->
+<?php endif; ?>
