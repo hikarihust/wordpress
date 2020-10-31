18b0a87d081ab6e1bb5ed6670635f7b1a9e01f94
vudinhquang <vudinhquangk53@gmail.com>
2020-10-31 22:34:11 +0900

    Check form 3

--- wp-content/plugins/zendvn-myplugin/admin.php
+++ wp-content/plugins/zendvn-myplugin/admin.php
@@ -4,11 +4,17 @@ require_once ZENDVN_MP_PLUGIN_DIR . '/includes/support.php';
 class ZendvnMpAdmin{	
 	
 	public function __construct(){		
-		$this->ajaxPage();
+		// $this->ajaxPage();
+		$this->ajaxPage2();
 	}
 	
 	public function ajaxPage(){
 		require_once ZENDVN_MP_SETTING_DIR . '/ajax.php';
 		new Zendvn_Mp_Setting_Ajax();
+	}
+
+	public function ajaxPage2(){
+		require_once ZENDVN_MP_SETTING_DIR . '/ajax2.php';
+		new Zendvn_Mp_Setting_Ajax2();
 	}
 }
---
+++ wp-content/plugins/zendvn-myplugin/js/ajax2.js
@@ -0,0 +1,38 @@
+(function($){
+	$.fn.checkInput = function(phpFunc,btnID){
+		$(this).blur(function(e){
+			var dataObj = {				
+					"action"	: phpFunc,
+					 "value"	: $(this).val(),
+					 "inputID"	: $(this).attr("id")
+				};			
+			console.log(dataObj);
+			
+			var inputID = "#" + dataObj.inputID;
+			$.ajax({
+				url		: ajaxurl,//admin-ajax.php?action=zendvn_check_form
+				type	: "POST",
+				data	: dataObj,
+				dataType: "json",
+				success	: function(data, status, jsXHR){
+							
+							// console.log(data);
+							$(inputID).next('span').remove();
+							if(data.status == false){
+								$("#" + btnID).attr('disabled','disabled');
+								$(inputID)
+									.after('<span>' + data.errors.msg + '</span>');
+							}else{
+								$("#" + btnID).removeAttr('disabled');
+								$(inputID)
+									.after('<span>OK</span>');
+							}
+						}
+			});
+		});
+	};
+}(jQuery));
+
+jQuery(document).ready(function($){
+	$("#zendvn_mp_st_ajax2_title").checkInput("zendvn_check_form2","btn-save-change");
+});
