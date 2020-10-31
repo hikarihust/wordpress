d3c7e42829078f763f091b21012a7defbd80c383
vudinhquang <vudinhquangk53@gmail.com>
2020-11-01 01:27:44 +0900

    Template Tags 1

--- wp-content/themes/tmp/the loop/twentyten-child/template tags/index.php
+++ wp-content/themes/tmp/the loop/twentyten-child/template tags/index.php
@@ -14,34 +14,17 @@
  */
 
 get_header(); ?>
-<style>
-#zendvn-mp-info{
-	background-color: white;
-	min-height: 300px;
-	border: solid 1px #ccc;
-	padding: 10px;
-}
-</style>
+
 		<div id="container">
 			<div id="content" role="main">
-			<div id="zendvn-mp-info">
-				<ul>
-					<li>get_search_form()		: <?php echo get_search_form();?> </li>
-					<li>wp_loginout()			: <?php echo wp_loginout();?> </li>
-					<li>wp_logout_url()			: <?php echo wp_logout_url();?> </li>
-					<li>wp_login_url()			: <?php echo wp_login_url();?> </li>
-					<li>wp_login_form()			: <?php echo wp_login_form();?> </li>
-					<li>wp_lostpassword_url()	: <?php echo wp_lostpassword_url();?> </li>
-					<li>wp_register()			: <?php echo wp_register();?> </li>
-				</ul>
-			</div>
+
 			<?php
 			/*
 			 * Run the loop to output the posts.
 			 * If you want to overload this in a child theme then include a file
 			 * called loop-index.php and that will be used instead.
 			 */
-			//get_template_part( 'loop', 'index' );
+			get_template_part( 'loop', 'index' );
 			?>
 			</div><!-- #content -->
 		</div><!-- #container -->
