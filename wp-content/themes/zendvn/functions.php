cede4bf9ff8bf8540254d9bf2784dee86a2b5450
vudinhquang <vudinhquangk53@gmail.com>
2020-11-10 00:32:18 +0900

    Nhúng CSS 2

--- wp-content/themes/zendvn/functions.php
+++ wp-content/themes/zendvn/functions.php
@@ -4,16 +4,8 @@
 <script src="js/html5.js" type="text/javascript"></script>
 <![endif]-->
 <meta name='robots' content='noindex,follow' />
-<link rel='stylesheet' href='http://wordpress.xyz/wp-content/themes/zendvn/css/symple_shortcodes_styles.css' type='text/css' media='all' />
-<link rel='stylesheet' href='http://wordpress.xyz/wp-content/themes/zendvn/css/style.css' type='text/css' media='all' />
-<link rel='stylesheet' href='http://wordpress.xyz/wp-content/themes/zendvn/css/responsive.css' type='text/css' media='all' />
-<link rel='stylesheet' href='http://wordpress.xyz/wp-content/themes/zendvn/css/site.css' type='text/css' media='all' />
 <script type='text/javascript' src='http://wordpress.xyz/wp-content/themes/zendvn/js/jquery/jquery.js'></script>
 <script type='text/javascript' src='http://wordpress.xyz/wp-content/themes/zendvn/js/jquery/jquery-migrate.min.js'></script>
-<!--[if IE 8]>
-<link rel="stylesheet" type="text/css" href="css/ie8.css" media="screen">
-<![endif]-->
-*/
 
 /*============================================================================
  * 1. NẠP NHỮNG TẬP TIN CSS VÀO THEME
@@ -21,6 +13,22 @@
  add_action('wp_enqueue_scripts', 'zendvn_theme_register_style');
 
  function zendvn_theme_register_style(){
+    global $wp_styles;
      $cssUrl = get_template_directory_uri() . '/css';
-     echo $cssUrl;
+    //  echo $cssUrl;
+	wp_register_style('zendvn_theme_symple_shortcodes', $cssUrl . '/symple_shortcodes_styles.css',array(),'1.0');
+	wp_enqueue_style('zendvn_theme_symple_shortcodes');
+	
+	wp_register_style('zendvn_theme_style', $cssUrl . '/style.css',array(),'1.0');
+	wp_enqueue_style('zendvn_theme_style');
+	
+	wp_register_style('zendvn_theme_responsive', $cssUrl . '/responsive.css',array(),'1.0');
+	wp_enqueue_style('zendvn_theme_responsive');
+	
+	wp_register_style('zendvn_theme_site', $cssUrl . '/site.css',array(),'1.0');
+	wp_enqueue_style('zendvn_theme_site');
+	
+	wp_register_style('zendvn_theme_ie8', $cssUrl . '/ie8.css',array(),'1.0');
+    $wp_styles->add_data('zendvn_theme_ie8', 'conditional', 'IE 8');
+	wp_enqueue_style('zendvn_theme_ie8');
  }
