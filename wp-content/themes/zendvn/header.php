eb687a6fa7d33a097c7c3baf8d23bf958423534a
vudinhquang <vudinhquangk53@gmail.com>
2020-11-09 13:58:31 +0900

    Tách khối giao diện 2

--- wp-content/themes/zendvn/header.php
+++ wp-content/themes/zendvn/header.php
@@ -35,52 +35,8 @@
 							<span class="fa fa-clock-o"></span>13-Oct-2014
 						</div>
 					</div>
-					<!-- .topbar-date -->
-					<div id="topbar-nav" class="cr">
-						<div class="menu-menu-container">
-							<ul id="menu-menu" class="top-nav sf-menu">
-								<li>
-									<a href="#">Home</a>
-								</li>
-								<li class="dropdown">
-									<a href="#">Features <i class="fa fa-caret-down nav-arrow"></i></a>
-									<ul class="sub-menu">
-										<li>
-											<a href="#">Standard Post</a>
-										</li>
-										<li>
-											<a href="#">Gallery</a>
-										</li>
-										<li>
-											<a href="#">Audio</a>
-										</li>
-										<li>
-											<a href="#">Video</a>
-										</li>
-										<li>
-											<a href="#">Symple Shortcodes</a>
-										</li>
-										<li>
-											<a href="#">Contributors</a>
-										</li>
-									</ul></li>
-								<li>
-									<a href="#">Archives</a>
-								</li>
-								<li>
-									<a href="#">Contact</a>
-								</li>
-								<li>
-									<a target="_blank" href="#">Customize</a>
-								</li>
-								<li>
-									<a href="#" class="nav-loginout-link">
-										<span class="fa fa-lock"></span> Login
-									</a>
-								</li>
-							</ul>
-						</div>
-					</div>
+                    <!-- .topbar-date -->
+                    <?php get_sidebar('top-menu') ?>
 					<!-- #topbar-nav -->
 					<div id="topbar-search" class="clr">
 						<form method="get" class="topbar-searchform" action="#" role="search">
@@ -117,46 +73,7 @@
 				</div>
 				<!-- .ad-spot -->
 			</header>
-			<!-- #header -->
-			<div id="site-navigation-wrap" class="clr">
-				<div id="site-navigation-inner" class="clr container">
-					<nav id="site-navigation" class="navigation main-navigation clr" role="navigation">
-						<div class="menu-categories-container">
-							<ul id="menu-categories" class="main-nav dropdown-menu sf-menu">
-								<li class="menu-item-object-category cat-28">
-									<a href="#">Sports</a>
-								</li>
-								<li class="menu-item-object-category cat-5">
-									<a href="#">Photography</a>
-								</li>
-								<li class="menu-item-object-category cat-6">
-									<a href="#">Travel</a>
-								</li>
-								<li class="menu-item-object-category cat-3">
-									<a href="#">Shopping</a>
-								</li>
-								<li class="menu-item-object-category cat-4">
-									<a href="#">Nature</a>
-								</li>
-								<li class="menu-item-object-category cat-27">
-									<a href="#">News</a>
-								</li>
-								<li class="menu-item-object-category cat-2">
-									<a href="#">Videos</a>
-								</li>
-								<li class="menu-item-object-category cat-26">
-									<a href="#">Health</a>
-								</li>
-							</ul>
-						</div>
-						<a href="#mobile-nav" class="navigation-toggle">
-							<span class="fa fa-bars navigation-toggle-icon"></span>
-							<span class="navigation-toggle-text">Browse Categories</span>
-						</a>
-					</nav>
-					<!-- #site-navigation -->
-				</div>
-				<!-- #site-navigation-inner -->
-			</div>
+            <!-- #header -->
+            <?php get_sidebar('main-menu') ?>
 			<!-- #site-navigation-wrap -->
 		</div>
