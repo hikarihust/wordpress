f75ffc7c877ffd35342d80f01b35efffe83bb3bd
vudinhquang <vudinhquangk53@gmail.com>
2020-11-09 12:01:28 +0900

    Tách khối giao diện 1

---
+++ wp-content/themes/zendvn/header.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Spartan</title>
+	<!--[if lt IE 9]>
+			<script src="js/html5.js" type="text/javascript"></script>
+	<![endif]-->
+	<meta name='robots' content='noindex,follow' />
+	<link rel='stylesheet' href='http://wordpress.xyz/wp-content/themes/zendvn/css/symple_shortcodes_styles.css' type='text/css' media='all' />
+	<link rel='stylesheet' href='http://wordpress.xyz/wp-content/themes/zendvn/css/style.css' type='text/css' media='all' />
+	<link rel='stylesheet' href='http://wordpress.xyz/wp-content/themes/zendvn/css/responsive.css' type='text/css' media='all' />
+	<link rel='stylesheet' href='http://wordpress.xyz/wp-content/themes/zendvn/css/site.css' type='text/css' media='all' />
+	<script type='text/javascript' src='http://wordpress.xyz/wp-content/themes/zendvn/js/jquery/jquery.js'></script>
+	<script type='text/javascript' src='http://wordpress.xyz/wp-content/themes/zendvn/js/jquery/jquery-migrate.min.js'></script>
+	<!--[if IE 8]>
+		<link rel="stylesheet" type="text/css" href="css/ie8.css" media="screen">
+	<![endif]-->
+</head>
+
+<body class="home fixed-nav">
+
+	<div id="wrap" class="clr">
+
+		<div id="top-wrap" class="clr">
+			<div id="topbar" class="clr">
+				<div class="container clr">
+					<div id="topbar-date" class="clr">
+						<div class="topbar-date-full">
+							<span class="fa fa-clock-o"></span>Monday 13th October 2014
+						</div>
+						<div class="topbar-date-condensed">
+							<span class="fa fa-clock-o"></span>13-Oct-2014
+						</div>
+					</div>
+					<!-- .topbar-date -->
+					<div id="topbar-nav" class="cr">
+						<div class="menu-menu-container">
+							<ul id="menu-menu" class="top-nav sf-menu">
+								<li>
+									<a href="#">Home</a>
+								</li>
+								<li class="dropdown">
+									<a href="#">Features <i class="fa fa-caret-down nav-arrow"></i></a>
+									<ul class="sub-menu">
+										<li>
+											<a href="#">Standard Post</a>
+										</li>
+										<li>
+											<a href="#">Gallery</a>
+										</li>
+										<li>
+											<a href="#">Audio</a>
+										</li>
+										<li>
+											<a href="#">Video</a>
+										</li>
+										<li>
+											<a href="#">Symple Shortcodes</a>
+										</li>
+										<li>
+											<a href="#">Contributors</a>
+										</li>
+									</ul></li>
+								<li>
+									<a href="#">Archives</a>
+								</li>
+								<li>
+									<a href="#">Contact</a>
+								</li>
+								<li>
+									<a target="_blank" href="#">Customize</a>
+								</li>
+								<li>
+									<a href="#" class="nav-loginout-link">
+										<span class="fa fa-lock"></span> Login
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<!-- #topbar-nav -->
+					<div id="topbar-search" class="clr">
+						<form method="get" class="topbar-searchform" action="#" role="search">
+							<input type="search" class="field topbar-searchform-input" name="s" value="" placeholder="Type your search & hit enter" />
+							<button type="submit" class="topbar-searchform-btn">
+								<span class="fa fa-search"></span>
+							</button>
+						</form>
+					</div>
+					<!-- topbar-search -->
+				</div>
+				<!-- .container -->
+			</div>
+			<!-- #topbar -->
+			<header id="header" class="site-header clr container" role="banner">
+				<div class="site-branding clr">
+					<div id="logo" class="clr">
+						<div class="site-text-logo clr">
+							<h1>
+								<a href="#" title="Spartan" rel="home">Spartan</a>
+							</h1>
+						</div>
+					</div>
+					<!-- #logo -->
+					<div id="blog-description" class="clr">
+						Edit your subheading via the theme customizer. <br /> It looks much better when it's 2 lines long.
+					</div>
+					<!-- #blog-description -->
+				</div>
+				<!-- .site-branding -->
+				<div class="ad-spot header-ad clr">
+					<a href="#" title="Ad"><img
+						src="http://wordpress.xyz/wp-content/themes/zendvn/images/ad-620x80.png" alt="Ad" /></a>
+				</div>
+				<!-- .ad-spot -->
+			</header>
+			<!-- #header -->
+			<div id="site-navigation-wrap" class="clr">
+				<div id="site-navigation-inner" class="clr container">
+					<nav id="site-navigation" class="navigation main-navigation clr" role="navigation">
+						<div class="menu-categories-container">
+							<ul id="menu-categories" class="main-nav dropdown-menu sf-menu">
+								<li class="menu-item-object-category cat-28">
+									<a href="#">Sports</a>
+								</li>
+								<li class="menu-item-object-category cat-5">
+									<a href="#">Photography</a>
+								</li>
+								<li class="menu-item-object-category cat-6">
+									<a href="#">Travel</a>
+								</li>
+								<li class="menu-item-object-category cat-3">
+									<a href="#">Shopping</a>
+								</li>
+								<li class="menu-item-object-category cat-4">
+									<a href="#">Nature</a>
+								</li>
+								<li class="menu-item-object-category cat-27">
+									<a href="#">News</a>
+								</li>
+								<li class="menu-item-object-category cat-2">
+									<a href="#">Videos</a>
+								</li>
+								<li class="menu-item-object-category cat-26">
+									<a href="#">Health</a>
+								</li>
+							</ul>
+						</div>
+						<a href="#mobile-nav" class="navigation-toggle">
+							<span class="fa fa-bars navigation-toggle-icon"></span>
+							<span class="navigation-toggle-text">Browse Categories</span>
+						</a>
+					</nav>
+					<!-- #site-navigation -->
+				</div>
+				<!-- #site-navigation-inner -->
+			</div>
+			<!-- #site-navigation-wrap -->
+		</div>
