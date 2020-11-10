32deee543156df15beae1b3ef772606f06eb6dd2
vudinhquang <vudinhquangk53@gmail.com>
2020-11-10 14:12:22 +0900

    Widget Search 3

--- wp-content/themes/zendvn/inc/widgets/html/searchForm.php
+++ wp-content/themes/zendvn/inc/widgets/html/searchForm.php
@@ -1,8 +1,7 @@
-<form role="search" method="get" id="searchform" class="searchform" 
-    action="http://wordpress.xyz/">
-    <div>
-        <label class="screen-reader-text" for="s">Search for:</label>
-        <input type="text" value="" name="s" id="s">
-        <input type="submit" id="searchsubmit" value="Search">
-    </div>
+<form method="get" id="searchform" class="site-searchform" 
+    action="<?php echo site_url();?>" role="search">
+    <input type="search" class="field" name="s" value="" id="s" placeholder="Search...">
+    <button type="submit">
+        <span class="fa fa-search"></span>
+    </button>
 </form>
--- wp-content/themes/zendvn/inc/widgets/searchForm.php
+++ wp-content/themes/zendvn/inc/widgets/searchForm.php
@@ -17,18 +17,11 @@ class Zendvn_Theme_Widget_SearchForm extends WP_Widget {
 		extract($args);				
 				
 		$title = apply_filters('widget_title', $instance['title']);
-		$title = (empty($title))? 'Abc simple': $title;
-		$movie = (empty($instance['movie']))? '&nbsp;':$instance['movie'];
-		$song = (empty($instance['song']))? '&nbsp;':$instance['song'];
-		$css = (empty($instance['css']))? '':$instance['css'];
-		$classname = $this->widget_options['classname'];
-		$before_widget = str_replace($classname, $classname . ' ' . $css, $before_widget);
 		echo $before_widget;
-		echo $before_title . $title . $after_title;		
-		echo '<ul>';
-		echo '<li>Fav Movie: ' . $movie . '</li>';
-		echo '<li>Fav Song: ' . $song . '</li>';
-		echo '</ul>';
+		if(!empty($title)){
+			echo $before_title . $title . $after_title;		
+		}
+		require ZENDVN_THEME_WIDGET_DIR . '/html/searchForm.php';
 		echo $after_widget;
 	}
 
@@ -36,9 +29,6 @@ class Zendvn_Theme_Widget_SearchForm extends WP_Widget {
 		$instance = $old_instance;
 		
 		$instance['title'] = strip_tags($new_instance['title']);
-		$instance['movie'] = strip_tags($new_instance['movie']);
-		$instance['song'] = strip_tags($new_instance['song']);
-		$instance['css'] = strip_tags($new_instance['css']);
 		
 		return $instance;
 	}
@@ -51,35 +41,8 @@ class Zendvn_Theme_Widget_SearchForm extends WP_Widget {
 		$inputName 	= $this->get_field_name('title');
 		$inputValue = @$instance['title'];
 		$arr = array('class' =>'widefat','id' => $inputID);			
-		echo '<p><label for="' . $inputID . '">'. translate('Title') .'</label>'
-		. $htmlObj->textbox($inputName, $inputValue, $arr) 
-		. '</p>';
-
-		//Tao phan tu chua Movie
-		$inputID 	= $this->get_field_id('movie');
-		$inputName 	= $this->get_field_name('movie');
-		$inputValue = @$instance['movie'];
-		$arr = array('class' =>'widefat','id' => $inputID);
-		echo '<p><label for="' . $inputID . '">' . translate('Movie') . '</label>'
-		. $htmlObj->textbox($inputName,$inputValue,$arr)
-		. '</p>';
-
-		//Tao phan tu chua Song
-		$inputID 	= $this->get_field_id('song');
-		$inputName 	= $this->get_field_name('song');
-		$inputValue = @$instance['song'];
-		$arr = array('class' =>'widefat','id' => $inputID);
-		echo '<p><label for="' . $inputID . '">' . translate('Song') . '</label>'
-		. $htmlObj->textbox($inputName,$inputValue,$arr)
-		. '</p>';
-
-		//Tao phan tu chua CSS
-		$inputID 	= $this->get_field_id('css');
-		$inputName 	= $this->get_field_name('css');
-		$inputValue = @$instance['css'];
-		$arr = array('class' =>'widefat','id' => $inputID);
-		echo '<p><label for="' . $inputID . '">' . translate('Css class') . '</label>'
-		. $htmlObj->textbox($inputName,$inputValue,$arr)
-		. '</p>';
+		$html		= 	$htmlObj->label(translate('Title'),array('for'=>$inputID))
+				. $htmlObj->textbox($inputName,$inputValue,$arr); 
+		echo $htmlObj->pTag($html);
 	}
 }
