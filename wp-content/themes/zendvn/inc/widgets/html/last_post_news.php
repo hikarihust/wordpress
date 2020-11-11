174a453497ef2b9a9d5cc1130f6c0d60f2088e94
vudinhquang <vudinhquangk53@gmail.com>
2020-11-12 03:15:54 +0900

    Widget right last post 2

---
+++ wp-content/themes/zendvn/inc/widgets/html/last_post_news.php
@@ -0,0 +1,29 @@
+
+    <ul class="widget-latest-news clr">
+        <li class="first-post clr">
+            <div class="first-post-media clr">
+                <a href="#" title="I&#8217;m Hungry, Make Me A Sandwich"> 
+                    <img src="http://wordpress.xyz/wp-content/themes/zendvn/files/uploads/2013/12/shutterstock_167357615-620x350.jpg" alt="I&#8217;m Hungry, Make Me A Sandwich" width="620" height="350" />
+                </a>
+                <div class="entry-cat-tag cat-36-bg">
+                    <a href="category/health/food/index.html" title="Food">Food</a>
+                </div>
+                <!-- .entry-cat-tag -->
+            </div>
+            <!-- .first-post-media --> 
+            <a href="#" title="I&#8217;m Hungry, Make Me A Sandwich" class="widget-recent-posts-title">I&#8217;m Hungry, Make Me A Sandwich</a>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nec diam non velit vestibulum sagittis.&hellip;</p>
+        </li>
+        <li>
+            <a href="#" title="Best Places To Buy Music">Best Places To Buy Music</a>
+        </li>
+        <li>
+            <a href="#" title="Speeding Down The Formula 1 Track">Speeding Down The Formula 1 Track</a>
+        </li>
+        <li>
+            <a href="#" title="Run&#8230;Hes Coming To Get You!">Run&#8230;Hes Coming To Get You!</a>
+        </li>
+        <li>
+            <a href="#" title="Online Fashion Shopping Tips">Online Fashion Shopping Tips</a>
+        </li>
+    </ul>
