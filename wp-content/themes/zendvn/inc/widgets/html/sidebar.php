29863420d0d978e5c577680134defd557fabd017
vudinhquang <vudinhquangk53@gmail.com>
2020-11-11 14:11:22 +0900

    Widget Right Slider 2

--- wp-content/themes/zendvn/inc/widgets/html/sidebar.php
+++ wp-content/themes/zendvn/inc/widgets/html/sidebar.php
@@ -5,10 +5,22 @@
     <?php 
         while ($wpQuery->have_posts()){
             $wpQuery->the_post();
+			$postObj = $wpQuery->post;
+			//echo $postObj->ID;
+			$feature_img = wp_get_attachment_url(get_post_thumbnail_id($postObj->ID));
+			if($feature_img == false){
+				$imgUrl = $this->get_img_url($postObj->post_content); 
+			}else{
+				$imgUrl = $feature_img;
+			}
+			//get_post_thumbnail_id($postObj->ID);
+			if(!empty($imgUrl)){
+				$imgUrl = $this->get_new_img_url($imgUrl, $width, $height);	
+			}
     ?>
     <div class="slider-widget-slide clr">
-        <a href="#" title="<?php the_title();?>" class="widget-recent-posts-thumbnail clr">
-            <img src="http://wordpress.xyz/wp-content/themes/zendvn/files/uploads/2014/02/shutterstock_80791570-620x350.jpg" 
+        <a href="<?php the_permalink();?>" title="<?php the_title();?>" class="widget-recent-posts-thumbnail clr">
+            <img src="<?php echo $imgUrl;?>" 
                 alt="<?php the_title();?>" width="620" height="350" />
             <div class="slider-widget-title"><?php the_title();?></div>
         </a>
