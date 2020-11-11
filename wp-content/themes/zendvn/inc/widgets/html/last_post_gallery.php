2214755818e642dc1644f6e8b080c7c2502b8491
vudinhquang <vudinhquangk53@gmail.com>
2020-11-12 04:01:25 +0900

    Widget footer last post news

--- wp-content/themes/zendvn/inc/widgets/html/last_post_gallery.php
+++ wp-content/themes/zendvn/inc/widgets/html/last_post_gallery.php
@@ -1,2 +1,38 @@
-<?php
-echo __FILE__;
+<?php 
+	if($wpQuery->have_posts()){
+?>
+ <ul class="widget-recent-posts clr">
+    <?php 
+	$width = 238;
+	$height = 91;
+    while ($wpQuery->have_posts()){
+        $wpQuery->the_post();
+        $postObj = $wpQuery->post;
+        //echo $postObj->ID;
+        $feature_img = wp_get_attachment_url(get_post_thumbnail_id($postObj->ID));
+        if($feature_img == false){
+            $imgUrl = $this->get_img_url($postObj->post_content); 
+        }else{
+            $imgUrl = $feature_img;
+        }
+        //get_post_thumbnail_id($postObj->ID);
+        if(!empty($imgUrl)){
+            $imgUrl = $this->get_new_img_url($imgUrl, $width, $height);	
+        }
+    ?>
+    <li class="clr widget-recent-posts-li top-thumbnail format-gallery">
+        <a href="<?php the_permalink();?>" title="<?php the_title();?>" class="widget-recent-posts-thumbnail clr"> 
+            <img src="<?php echo $imgUrl;?>" alt="<?php the_title();?>" width="650" height="250" />
+        </a>
+        <div class="widget-recent-posts-content clr">
+            <a href="<?php the_permalink();?>" title="<?php the_title();?>" class="widget-recent-posts-title"><?php the_title();?></a>
+        </div>
+        <!-- .widget-recent-posts-content -->
+    </li>
+    <?php 
+		}//while ($wpQuery->have_posts()){
+    ?>
+</ul>
+<?php 
+    }//while ($wpQuery->have_posts()){
+?>
--- wp-content/themes/zendvn/inc/widgets/html/last_post_news.php
+++ wp-content/themes/zendvn/inc/widgets/html/last_post_news.php
@@ -1,29 +1,55 @@
-
-    <ul class="widget-latest-news clr">
-        <li class="first-post clr">
-            <div class="first-post-media clr">
-                <a href="#" title="I&#8217;m Hungry, Make Me A Sandwich"> 
-                    <img src="http://wordpress.xyz/wp-content/themes/zendvn/files/uploads/2013/12/shutterstock_167357615-620x350.jpg" alt="I&#8217;m Hungry, Make Me A Sandwich" width="620" height="350" />
-                </a>
-                <div class="entry-cat-tag cat-36-bg">
-                    <a href="category/health/food/index.html" title="Food">Food</a>
-                </div>
-                <!-- .entry-cat-tag -->
+<?php 
+	if($wpQuery->have_posts()){
+?>
+<ul class="widget-latest-news clr">
+    <?php 
+        $width			= 238;
+        $height			= 134;
+        $i = 1;
+        while ($wpQuery->have_posts()){
+            $wpQuery->the_post();
+			$postObj = $wpQuery->post;
+			//echo $postObj->ID;
+    ?>
+    <?php if($i== 1):?>
+    <?php
+    $feature_img = wp_get_attachment_url(get_post_thumbnail_id($postObj->ID));
+    if($feature_img == false){
+        $imgUrl = $this->get_img_url($postObj->post_content); 
+    }else{
+        $imgUrl = $feature_img;
+    }
+    //get_post_thumbnail_id($postObj->ID);
+    if(!empty($imgUrl)){
+        $imgUrl = $this->get_new_img_url($imgUrl, $width, $height);	
+    }
+    ?>
+    <li class="first-post clr">
+        <div class="first-post-media clr">
+            <a href="<?php the_permalink();?>" title="<?php the_title();?>"> 
+                <img src="<?php echo $imgUrl;?>" alt="<?php the_title();?>" width="620" height="350" />
+            </a>
+            <?php if($cat !=0):?>
+            <div class="entry-cat-tag cat-36-bg">
+                <a href="<?php echo get_category_link($cat)?>" title="<?php echo get_cat_name($cat)?>"><?php echo get_cat_name($cat)?></a>
             </div>
-            <!-- .first-post-media --> 
-            <a href="#" title="I&#8217;m Hungry, Make Me A Sandwich" class="widget-recent-posts-title">I&#8217;m Hungry, Make Me A Sandwich</a>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nec diam non velit vestibulum sagittis.&hellip;</p>
-        </li>
-        <li>
-            <a href="#" title="Best Places To Buy Music">Best Places To Buy Music</a>
-        </li>
-        <li>
-            <a href="#" title="Speeding Down The Formula 1 Track">Speeding Down The Formula 1 Track</a>
-        </li>
-        <li>
-            <a href="#" title="Run&#8230;Hes Coming To Get You!">Run&#8230;Hes Coming To Get You!</a>
-        </li>
-        <li>
-            <a href="#" title="Online Fashion Shopping Tips">Online Fashion Shopping Tips</a>
-        </li>
-    </ul>
+            <?php endif;?>
+            <!-- .entry-cat-tag -->
+        </div>
+        <!-- .first-post-media --> 
+        <a href="<?php the_permalink();?>" title="<?php the_title();?>" class="widget-recent-posts-title"><?php the_title();?></a>
+        <p><?php echo mb_substr(get_the_excerpt(), 0,65) . '...';?></p>
+    </li>
+    <?php else :?>
+    <li>
+        <a href="<?php the_permalink();?>" title="<?php the_title();?>"><?php echo mb_substr(get_the_title(), 0,32) . '...';?></a>
+    </li>
+    <?php endif;?>
+    <?php 
+        $i++;
+		}//while ($wpQuery->have_posts()){
+    ?>
+</ul>
+<?php 
+    }//while ($wpQuery->have_posts()){
+?>
