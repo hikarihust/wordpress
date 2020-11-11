d738849fa481f32cfec869cc449a1a24267dc533
vudinhquang <vudinhquangk53@gmail.com>
2020-11-11 13:43:24 +0900

    Widget Right Slider 1

--- wp-content/themes/zendvn/inc/widgets/html/sidebar.php
+++ wp-content/themes/zendvn/inc/widgets/html/sidebar.php
@@ -1,2 +1,28 @@
-<?php
-echo __FILE__;
+<?php 
+	if($wpQuery->have_posts()){
+?>
+<div class="slider-widget owl-carousel clr">
+    <?php 
+        while ($wpQuery->have_posts()){
+            $wpQuery->the_post();
+    ?>
+    <div class="slider-widget-slide clr">
+        <a href="#" title="<?php the_title();?>" class="widget-recent-posts-thumbnail clr">
+            <img src="http://wordpress.xyz/wp-content/themes/zendvn/files/uploads/2014/02/shutterstock_80791570-620x350.jpg" 
+                alt="<?php the_title();?>" width="620" height="350" />
+            <div class="slider-widget-title"><?php the_title();?></div>
+        </a>
+        <?php if($cat != 0):?>
+		<div class="entry-cat-tag cat-29-bg">
+			<a href="<?php echo get_category_link($cat);?>" title="<?php echo get_cat_name($cat);?>"><?php echo get_cat_name($cat);?></a>
+		</div>
+        <?php endif;?>
+    </div>
+	<?php 
+		}//while ($wpQuery->have_posts()){
+	?>
+    <!-- .widget-slider-slide -->
+</div>
+<?php 
+    }//while ($wpQuery->have_posts()){
+?>
