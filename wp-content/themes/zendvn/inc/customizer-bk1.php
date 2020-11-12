2e7a791f9c12161e75afcbacfbff097f02990f49
vudinhquang <vudinhquangk53@gmail.com>
2020-11-13 01:51:20 +0900

    Custom control theme 3

---
+++ wp-content/themes/zendvn/inc/customizer-bk1.php
@@ -0,0 +1,42 @@
+<?php
+$tmp = get_theme_mods();
+
+echo '<pre>';
+print_r($tmp);
+echo '</pre>';
+echo '<pre>';
+print_r(get_theme_mod('zendvn_theme_general', array()));
+echo '</pre>';
+
+// echo '<br/>' . get_option('zendvn_theme_site_name');
+
+add_action('customize_register', 'zendvn_theme_customize_register');
+
+function zendvn_theme_customize_register($wp_customize) {
+	$wp_customize->add_section('zendvn_theme_general',array(
+        'title' => __('General'),
+        'description' => 'Hien thi cac phan tu trong Section',
+        'priority' => 20
+    ));
+
+	$wp_customize->add_setting('zendvn_theme_general[site_name]',array(
+        'default' 		=> 'ZendVN theme',
+        'capability' 	=>'edit_theme_options',
+        'type'			=> 'theme_mod',
+        'transport'		=> 'refresh',
+        'sanitize_callback' => 'sanitize_zendvn_theme_site_name'
+    ));
+
+	$wp_customize->add_control('zendvn_theme_site_name',array(
+        'label' 		=> __('Site name'),
+        'section' 		=> 'zendvn_theme_general',
+        'settings' 		=> 'zendvn_theme_general[site_name]',
+        'type'			=>'text'
+    ));
+}
+
+function sanitize_zendvn_theme_site_name($value){
+	$value = trim($value);
+	if(empty($value)) $value = 'Spartan';
+	return $value;
+}
--- wp-content/themes/zendvn/inc/customizer.php
+++ wp-content/themes/zendvn/inc/customizer.php
@@ -1,14 +1,51 @@
 <?php
+// $tmp = get_theme_mods();
+// echo '<pre>';
+// print_r($tmp);
+// echo '</pre>';
+
 add_action('customize_register', 'zendvn_theme_customize_register');
 
 function zendvn_theme_customize_register($wp_customize) {
-	$wp_customize->add_section('zendvn_theme_general',array(
+    $sectionID = 'zendvn_theme_general';
+	$wp_customize->add_section($sectionID,array(
         'title' => __('General'),
         'description' => 'Hien thi cac phan tu trong Section',
         'priority' => 20
     ));
 
-	$wp_customize->add_setting('zendvn_theme_general[site_name]',array(
+	//=======================================================
+	// TAO O RADIO
+	//=======================================================
+	$inputName = 'my-radio';
+	$settingID = $sectionID . '[' . $inputName . ']';
+	$wp_customize->add_setting($settingID,array(
+			'default' 		=> 'unknown',
+			'capability' 	=>'edit_theme_options',
+			'type'			=> 'theme_mod',
+			'transport'		=> 'postMessage',
+	));
+	
+	$controlID = 'zendvn-theme-' . $inputName;
+	$wp_customize->add_control($controlID,array(
+			'label' 		=> __('Gioi tinh'),
+			'section' 		=> $sectionID,
+			'settings' 		=> $settingID,
+			'type'			=>'radio',
+			'choices'		=> array(
+						'male' => 'Name',
+						'female' => 'Nu',
+						'unknown' => 'Chua xac dinh'
+					)
+	));
+
+	//=======================================================
+	// TAO O TEXTBOX
+    //=======================================================
+	$inputName = 'site-name';
+	$settingID = $sectionID . '[' . $inputName . ']';
+
+	$wp_customize->add_setting($settingID,array(
         'default' 		=> 'ZendVN theme',
         'capability' 	=>'edit_theme_options',
         'type'			=> 'theme_mod',
@@ -16,10 +53,11 @@ function zendvn_theme_customize_register($wp_customize) {
         'sanitize_callback' => 'sanitize_zendvn_theme_site_name'
     ));
 
-	$wp_customize->add_control('zendvn_theme_site_name',array(
+    $controlID = 'zendvn-theme-' . $inputName;
+	$wp_customize->add_control($controlID,array(
         'label' 		=> __('Site name'),
-        'section' 		=> 'zendvn_theme_general',
-        'settings' 		=> 'zendvn_theme_general[site_name]',
+        'section' 		=> $sectionID ,
+        'settings' 		=> $settingID ,
         'type'			=>'text'
     ));
 }
