fa9575df7f51e42c3d98b0d387078152a7414945
vudinhquang <vudinhquangk53@gmail.com>
2020-11-13 00:10:04 +0900

    Custom control theme 2

--- wp-content/themes/zendvn/inc/customizer.php
+++ wp-content/themes/zendvn/inc/customizer.php
@@ -7,4 +7,25 @@ function zendvn_theme_customize_register($wp_customize) {
         'description' => 'Hien thi cac phan tu trong Section',
         'priority' => 20
     ));
+
+	$wp_customize->add_setting('zendvn_theme_general[site_name]',array(
+        'default' 		=> 'ZendVN theme',
+        'capability' 	=>'edit_theme_options',
+        'type'			=> 'theme_mod',
+        'transport'		=> 'refresh',
+        'sanitize_callback' => 'sanitize_zendvn_theme_site_name'
+    ));
+
+	$wp_customize->add_control('zendvn_theme_site_name',array(
+        'label' 		=> __('Site name'),
+        'section' 		=> 'zendvn_theme_general',
+        'settings' 		=> 'zendvn_theme_general[site_name]',
+        'type'			=>'text'
+    ));
+}
+
+function sanitize_zendvn_theme_site_name($value){
+	$value = trim($value);
+	if(empty($value)) $value = 'Spartan';
+	return $value;
 }
