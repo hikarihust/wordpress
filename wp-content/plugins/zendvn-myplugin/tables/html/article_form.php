5554876856fa330d25dcf4cb854f8274b6545791
vudinhquang <vudinhquangk53@gmail.com>
2020-11-30 10:46:19 +0900

    Add action 2

--- wp-content/plugins/zendvn-myplugin/tables/html/article_form.php
+++ wp-content/plugins/zendvn-myplugin/tables/html/article_form.php
@@ -3,10 +3,29 @@ $htmlObj = new ZendvnHtml();
 $page = $_REQUEST['page'];
 $action = 'add';
 
-$title = $htmlObj->textbox('title','',array('class'=>'regular-text'));
+$msg = '';
+if (count(@$errors) > 0) {
+    $msg .= '<div class="error"><ul>';
+    foreach ($errors  as $key => $val) {
+        $msg .= '<li>' . $val . '</li>';
+    }
+    $msg .= '</ul></div>';
+
+    $vTitle     = @$_POST['title'];
+    $vPicture     = @$_POST['picture'];
+    $vContent     = @$_POST['content'];
+    $vStatus     = @$_POST['status'];
+}
+
+$title   = $htmlObj->textbox('title', @$vTitle, array('class' => 'regular-text'));
+$picture = $htmlObj->textbox('picture', @$vPicture, array('class' => 'regular-text'));
+$content = $htmlObj->textarea('content', @$vContent, array('rows' => 6, 'cols' => 60));
+$options['data'] = array('Inactive', 'Active');
+$status  = $htmlObj->selectbox('status', @$vStatus, array('class' => 'regular-text'), $options);
 ?>
 <div class="wrap">
     <h1>Add new an article</h1>
+    <?php echo $msg; ?>
     <form method="post" action="" novalidate="novalidate" id="<?php echo $page; ?>" enctype="multipart/form-data">
         <input type="hidden" name="action" value="<?php echo $action; ?>">
         <?php wp_nonce_field($action, 'security_code', false); ?>
@@ -18,7 +37,19 @@ $title = $htmlObj->textbox('title','',array('class'=>'regular-text'));
             <tbody>
                 <tr>
                     <th scope="row"><label for="title">Title</label></th>
-                    <td><?php echo $title;?></td>
+                    <td><?php echo $title; ?></td>
+                </tr>
+                <tr>
+                    <th scope="row"><label for="picture">Picture</label></th>
+                    <td><?php echo $picture; ?></td>
+                </tr>
+                <tr>
+                    <th scope="row"><label for="content">Content</label></th>
+                    <td><?php echo $content; ?></td>
+                </tr>
+                <tr>
+                    <th scope="row"><label for="status">Status</label></th>
+                    <td><?php echo $status; ?></td>
                 </tr>
             </tbody>
         </table>
--- wp-content/plugins/zendvn-myplugin/tables/my_article.php
+++ wp-content/plugins/zendvn-myplugin/tables/my_article.php
@@ -43,9 +43,36 @@ class Zendvn_Mp_Table_MyArticle{
 	}
     
     public function display_add() {
+		$security_code = @$_REQUEST['security_code'];
 		
+		if(wp_verify_nonce($security_code,'add')){
+			if(isset($_POST['title'])){
+				
+				//Kiem tra du lieu
+				$errors = $this->validate_form();
+
+				if(count($errors)>0){
+					require_once ZENDVN_MP_TABLES_DIR . '/html/article_form.php';
+				}else{
+				}
+			}
+			
+		}
 		require_once ZENDVN_MP_TABLES_DIR . '/html/article_form.php';
-    }
+	}
+	
+	private function validate_form(){
+		$errors = array();
+		if(empty($_POST['title'])){
+			$errors['title'] = "Title: Khong duoc de rong";
+		}
+		
+		if(empty($_POST['picture'])){
+			$errors['picture'] = "Picture: Khong duoc de rong";
+				
+		}
+		return $errors;
+	}
 
 	public function add_css_file(){
 		wp_register_style('zendvn_mp_tbl_article', ZENDVN_MP_CSS_URL . '/tbl_article.css',array(),'1.0');
