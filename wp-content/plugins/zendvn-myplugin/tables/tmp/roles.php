51358e01098df777ff9fac84db796d7325ccc48f
vudinhquang <vudinhquangk53@gmail.com>
2020-12-01 23:32:47 +0900

    User - Role - Cap 3

--- wp-content/plugins/zendvn-myplugin/tables/tmp/roles.php
+++ wp-content/plugins/zendvn-myplugin/tables/tmp/roles.php
@@ -4,19 +4,11 @@ new Zendvn_Mp_Roles();
 class Zendvn_Mp_Roles{
 	
 	public function __construct(){        
-        //WP_User
-        global $current_user;
-        $user = wp_get_current_user();
+        //WP_Role
+        $group = get_role('editor');
 
-        $user = new WP_User(2);
-        // $user->remove_role('contributor');
-        // $user->remove_role('editor');
-        // $user->add_role('contributor');
-        $user->__set('zendvn_mp_website', 'goole.com');
-
-        // $user->add_cap('manage_options', true);
-        echo '<pre>';
-        print_r($user);
-        echo '</pre>';
+        echo "<pre>";
+        print_r($group);
+        echo "</pre>";
 	}
 }
